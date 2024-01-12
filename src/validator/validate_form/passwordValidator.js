1b7e6c81fd6ba5d55a0ac8585cb9eef055a67607
mahdi <mahdimamashli1383@gmail.com>
2024-01-13 01:28:12 +0330

    Refactor `hasUtilsFunction.js` to use an object to store functions

--- src/validator/validate_form/passwordValidator.js
+++ src/validator/validate_form/passwordValidator.js
@@ -1,8 +1,5 @@
 function validatePassword(password){
-    const isUppercase = /(?=.*[A-Z])/
-    const isLowercase = /(?=.*[a-z])/
-    const isOneDigit = /(?=.*\d)/
-    const isSpecialCharacter = /(?=.*[@$!%*?&])/
+    
     const minLength = 8
     return password.length >= minLength
 }

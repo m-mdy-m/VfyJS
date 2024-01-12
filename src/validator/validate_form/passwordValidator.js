2a532341b9b7c161ff29bc5d02f79a02d4a7c913
mahdi <mahdimamashli1383@gmail.com>
2024-01-13 01:52:24 +0330

    Password validator function and test updates.

--- src/validator/validate_form/passwordValidator.js
+++ src/validator/validate_form/passwordValidator.js
@@ -1,8 +1,13 @@
-const hasValidate = require('../../utils/hasUtilsFunction')
-function validatePassword(password){
-    
-    const minLength = 8
-    return password.length >= minLength && hasValidate.hasLowerCase && hasValidate.hasUppercase && hasValidate.hasNumber && hasValidate.hasSpecialCharacter 
+const hasValidate = require("../../utils/hasUtilsFunction");
+function validatePassword(password) {
+  const minLength = 8;
+  return (
+    password.length >= minLength &&
+    hasValidate.hasLowerCase(password) &&
+    hasValidate.hasUppercase(password) &&
+    hasValidate.hasNumber(password) &&
+    hasValidate.hasSpecialCharacter(password)
+  );
 }
 
-module.exports = validatePassword
+module.exports = validatePassword;
--- tests/validator/TestForm/passwordValidator.test.js
+++ tests/validator/TestForm/passwordValidator.test.js
@@ -1,10 +1,13 @@
 const passwordValidator = require('../../../src/validator/validate_form/passwordValidator')
 
-
-test('validates a password with at least 8 characters', () => {
-    const validPassword = 'SecurePwd421421';
-    const invalidPassword = 'Pwd';
-  
+describe('validates a password with at least 8 characters', () => {
+  test('should return true for a valid password', () => {
+    const validPassword = 'SecurePwd123!';
     expect(passwordValidator(validPassword)).toBe(true);
+  });
+
+  test('should return false for an invalid password', () => {
+    const invalidPassword = 'Pwd';
     expect(passwordValidator(invalidPassword)).toBe(false);
   });
+});

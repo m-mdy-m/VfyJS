e36b27b6ebe3332cd623c262963a630041f5dd36
mahdi <mahdimamashli1383@gmail.com>
2024-01-14 22:19:14 +0330

    remove mocha and add jest

--- tests/validator/password.test.js
+++ tests/validator/password.test.js
@@ -1,8 +1,6 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
+function sum(a, b) {
+  return a + b;
+}
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });

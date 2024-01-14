80158cf3e9fa8c530d5192ff76d18a47302cdf62
mahdi <mahdimamashli1383@gmail.com>
2024-01-14 21:47:39 +0330

    rename folder and test mocha

--- test.js
+++
@@ -1,8 +0,0 @@
-const test = require('./src/validator/validate_form/passwordValidator')
-
-const a = test("MAHDI23!@4",{
-    minLength : 5,
-    maxLength : 20,
-    lowerCase : true,
-})
-console.log(a);

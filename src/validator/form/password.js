7f92ae7fb642faacbee9dce573e0aea4c497350e
mahdi <mahdimamashli1383@gmail.com>
2024-01-15 11:57:26 +0330

    Update password validation logic and error messages  and dynamic err message and config password

--- src/validator/form/password.js
+++ src/validator/form/password.js
@@ -3,39 +3,48 @@ const { MAX_LENGTH, MIN_LENGTH, trimmedValue } = require("../../common/validatio
 const hasOption = require("../../common/hasOption");
 const inputValidator = require("../../utils/inputValidator");
 const {handleValidationError} = require('../../errors/HandleError')
+
 function validatePassword(value, options = {}) {
   const has = inputValidator(value);
-  const upperCase = has.hasUppercase();
-  handleValidationError(upperCase,'Password must contain at least one uppercase letter.')
-  const lowerCase = has.hasLowerCase();
-  handleValidationError(lowerCase ,'Password must contain at least one lowerCase letter.')
-  const number = has.hasNumber();
-  handleValidationError(number ,'Password must have at least one number.')
-  const specialCharacter = has.hasSpecialCharacter();
-  handleValidationError(specialCharacter ,'The password must contain at least one special character such as (@#$%^&*).')
-  const Alphabetic = has.hasAlphabetic();
-  handleValidationError(specialCharacter ,'Input must contain at least one alphabetic character.')
-  const whitespace = !has.hasWhitespace(); 
-  if (!whitespace) {
+  const {
+    minLength = { value : has.hasMinLength(MIN_LENGTH), errorMessage: 'Password must be at least 8 characters long.' },
+    maxLength = { value : has.hasMaxLength(MAX_LENGTH), errorMessage: 'Password cannot exceed 20 characters.' },
+    uppercase = { required: true, errorMessage: 'Password must contain at least one uppercase letter.' },
+    lowercase = { required: true, errorMessage: 'Password must contain at least one lowercase letter.' },
+    number = { required: true, errorMessage: 'Password must have at least one number.' },
+    specialCharacter = { required: true, errorMessage: 'Password must contain at least one special character such as (@#$%^&*).' },
+    alphabetic = { required: true, errorMessage: 'Input must contain at least one alphabetic character.' },
+    whitespace = { required: false, errorMessage: 'Password cannot contain whitespace.' },
+  } = options;
+
+  handleValidationError(uppercase.required ? has.hasUppercase() : false , uppercase.errorMessage)
+  handleValidationError(lowercase.required ? has.hasLowerCase() : false , lowercase.errorMessage)
+  handleValidationError(number.required ? has.hasNumber() : false , number.errorMessage)
+  handleValidationError(specialCharacter.required ? has.hasSpecialCharacter() : false , specialCharacter.errorMessage)
+  handleValidationError(alphabetic.required ? has.hasAlphabetic() : false , alphabetic.errorMessage)
+  const whitespaceCheck = whitespace.required ? has.hasWhitespace() : !has.hasWhitespace();
+  if (!whitespaceCheck) {
     return value = trimmedValue(value);
-  }
-  const minLength = has.hasMinLength(MIN_LENGTH);
-  const maxLength = has.hasMaxLength(MAX_LENGTH);
-  if (typeof MIN_LENGTH !== 'number' || typeof MAX_LENGTH !== 'number') {
-    throw new Error('MIN_LENGTH and MAX_LENGTH must be of type number');
-  }
-  if (value.length < MIN_LENGTH || value.length > MAX_LENGTH) {
-    throw new Error(`Password length must be between ${MIN_LENGTH} and ${MAX_LENGTH} characters.`);
+  } 
+  if (typeof MIN_LENGTH !== 'number' || typeof MAX_LENGTH !== 'number' && value.length < MIN_LENGTH || value.length > MAX_LENGTH) {
+    if(minLength.errorMessage){
+      throw new Error(minLength.errorMessage)
+    }else if(maxLength.errorMessage){
+      throw new Error(maxLength.errorMessage)
+    }else{
+      throw new Error('MIN_LENGTH and MAX_LENGTH must be of type number and Password length must be between ${MIN_LENGTH} and ${MAX_LENGTH} characters.');
+    }
   }
   const isValid =
     minLength &&
     maxLength &&
-    upperCase &&
-    lowerCase &&
-    number &&
-    specialCharacter &&
-    Alphabetic &&
-    whitespace;
+    (uppercase.required ? has.hasUppercase() : true) &&
+    (lowercase.required ? has.hasLowerCase() : true) &&
+    (number.required ? has.hasNumber() : true) &&
+    (specialCharacter.required ? has.hasSpecialCharacter() : true) &&
+    (alphabetic.required ? has.hasAlphabetic() : true) &&
+    whitespaceCheck
+
   return isValid;
 }
 const result = validatePassword("    NASDHJsSDA231@#!@#asdsadsad   asdsa    SADS@#@asda      ")

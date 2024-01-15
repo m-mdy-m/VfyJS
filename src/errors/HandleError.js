c8961e5937b2f64c8a558bda0bfa7fcf8299084a
mahdi <mahdimamashli1383@gmail.com>
2024-01-15 10:37:01 +0330

    Add password validation and error handling.

--- src/errors/HandleError.js
+++ src/errors/HandleError.js
@@ -1,6 +1,21 @@
-function boolean(index){
-    if(typeof index !== 'boolean'){
-        throw new Error("")
-    }
-    return true;
+function boolean(index) {
+  if (typeof index !== "boolean") {
+    throw new Error("");
+  }
+  return true;
 }
+class ErrorMessage extends Error {
+  constructor(property, message) {
+    super(message);
+    this.property = property;
+  }
+}
+
+exports.handleValidationError = (property, message) => {
+  if (!property) {
+    throw new ErrorMessage(property, "Custom error message");
+  }
+  // Optionally, return something indicating success or additional information.
+  // For example, you might return the validated property.
+  return property;
+};
--- src/validator/form/password.js
+++ src/validator/form/password.js
@@ -2,16 +2,29 @@
 const { MAX_LENGTH, MIN_LENGTH } = require("../../common/validationConstants");
 const hasOption = require("../../common/hasOption");
 const inputValidator = require("../../utils/inputValidator");
+const {handleValidationError} = require('../../errors/HandleError')
 function validatePassword(value, options = {}) {
   const has = inputValidator(value);
   const upperCase = has.hasUppercase();
+  handleValidationError(upperCase,'Password must contain at least one uppercase letter.')
   const lowerCase = has.hasLowerCase();
+  handleValidationError(lowerCase ,'Password must contain at least one lowerCase letter.')
   const number = has.hasNumber();
   const specialCharacter = has.hasSpecialCharacter();
   const Alphabetic = has.hasAlphabetic();
   const whitespace = !has.hasWhitespace(); 
   const minLength = has.hasMinLength(MIN_LENGTH);
   const maxLength = has.hasMaxLength(MAX_LENGTH);
+  console.log("upperCase:", upperCase);
+  console.log("lowerCase:", lowerCase);
+  console.log("number:", number);
+  console.log("specialCharacter:", specialCharacter);
+  console.log("Alphabetic:", Alphabetic);
+  console.log("whitespace:", whitespace);
+  console.log("minLength:", minLength);
+  console.log("maxLength:", maxLength);
+  console.log("Password Length:", value.length);
+  console.log("Whitespace Check:", whitespace);
   const isValid =
     minLength &&
     maxLength &&
@@ -23,4 +36,10 @@ function validatePassword(value, options = {}) {
     whitespace;
   return isValid;
 }
+const a = 'adwadaw sa as'
+const v = a.toUpperCase(a)
+console.log('v =>', v);
+const result = validatePassword("NASDHJsSDA@242@#@#$%")
+console.log("Validation Result:", result);
+
 module.exports = validatePassword;

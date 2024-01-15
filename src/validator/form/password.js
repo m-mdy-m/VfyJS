16d03f5d5bbd681e4e0daaf9d7bdcfb8fbeb0fa1
mahdi <mahdimamashli1383@gmail.com>
2024-01-15 08:10:45 +0330

    Update password validation function and test

--- src/validator/form/password.js
+++ src/validator/form/password.js
@@ -1,10 +1,33 @@
-'use strict';
-const { MAX_LENGTH , MIN_LENGTH } = require('../../common/validationConstants')
-const hasOption = require('../../utils/inputValidator')
-const inputValidator = require('../../utils/inputValidator')
-function validatePassword(value,options){
-  const minLength = MIN_LENGTH
-  const maxLength = MAX_LENGTH
-  const has = inputValidator(value)
-  
-}reset
+"use strict";
+const { MAX_LENGTH, MIN_LENGTH } = require("../../common/validationConstants");
+const hasOption = require("../../utils/inputValidator");
+const inputValidator = require("../../utils/inputValidator");
+function validatePassword(value, options= {}) {
+  const minLength =  MIN_LENGTH;
+  const maxLength =  MAX_LENGTH;
+  const has = inputValidator(value);
+  const upperCase = has.hasUppercase();
+  const lowerCase = has.hasLowerCase();
+  const number = has.hasNumber();
+  const specialCharacter = has.hasSpecialCharacter();
+  const string = has.hasString();
+  console.log("upperCase =>", upperCase);
+  console.log("lowerCase =>", lowerCase);
+  console.log("number =>", number);
+  console.log("specialCharacter =>", specialCharacter);
+  console.log("string =>", string);
+  console.log("value =>", value);
+  console.log("options =>", options);
+  return { 
+    value.length > minLength &&
+    value.length < maxLength &&
+    upperCase &&
+    lowerCase &&
+    number &&
+    specialCharacter &&
+    string
+  }
+}
+validatePassword("MAhdia@242@#@#$%")
+
+module.exports = validatePassword
--- tests/validator/password.test.js
+++ tests/validator/password.test.js
@@ -1,6 +1,6 @@
 const password = require("../../src/validator/form/password");
 test("valid password", () => {
-  const isValid = password("MAHDI@#21312@");
+  const isValid = password("MAhdia@242@#@#$%");
   console.log("isValid:", isValid); 
 
   expect(isValid).toBe(true);

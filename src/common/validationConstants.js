74ff7cadb9e15c924acceeefe17efd97c9c01573
mahdi <mahdimamashli1383@gmail.com>
2024-01-15 11:02:09 +0330

    add constants teimmedValue

--- src/common/validationConstants.js
+++ src/common/validationConstants.js
@@ -1,3 +1,4 @@
 const MIN_LENGTH = 8
 const MAX_LENGTH = 64
-module.exports = {MIN_LENGTH ,MAX_LENGTH}
+const trimmedValue = (value)=>value.replace(/\s/g,'').trim()
+module.exports = {MIN_LENGTH ,MAX_LENGTH,trimmedValue}

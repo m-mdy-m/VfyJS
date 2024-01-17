07b78b29dd448eb021e721cc3dccf94ecf4f82a8
mahdi <mahdimamashli1383@gmail.com>
2024-01-17 15:33:24 +0330

    add handle error and fix bugs

--- src/validator/form/password.js
+++ src/validator/form/password.js
@@ -61,9 +61,9 @@
  */
 
 
-const { MAX_LENGTH, MIN_LENGTH, trimmedValue,getValidValue,getRequired , isValue } = require("../../common/validationConstants");
+const { MAX_LENGTH, MIN_LENGTH,getValidValue , isValue, getFalseRequired, getRequired } = require("../../common/validationConstants");
 const inputValidator = require("../../utils/inputValidator");
-const {ifFalsyValue,validateIfBothTruthy , ifTruthyValue, validatePropertyLengthAndType, isTypeMismatch} = require('../../errors/HandleError')
+const {ifFalsyValue , ifTruthyValue, validatePropertyLengthAndType, isTypeMismatch, validateIfBothTruthy} = require('../../errors/HandleError')
 const createValidationOptions = require('../../utils/handleOption')
 /**
  * Validates a password based on the provided options.
@@ -140,8 +140,8 @@ const msgError = [
   ifFalsyValue(specialCharacter.required ? validator.hasSpecialCharacter() : true , specialCharacter.errorMessage)
   ifFalsyValue(alphabetic.required ? validator.hasAlphabetic() : true , alphabetic.errorMessage)
   // Check and trim whitespace if necessary
-  let whitespaceCheck = getRequired(whitespace,false)
-  validateIfBothTruthy(whitespaceCheck , validator.hasWhitespace("Whitespace is not allowed. Please remove any leading or trailing spaces."),)
+  let whitespaceCheck = getFalseRequired(whitespace)
+  validateIfBothTruthy(!whitespaceCheck,validator.hasWhitespace(),"Whitespace is not allowed. Please remove any leading or trailing spaces.")
   const minValidLength  = getValidValue(minLength,MIN_LENGTH)
   const maxValidLength  = getValidValue(maxLength,MAX_LENGTH)
   let min = isValue(minLength,minValidLength)

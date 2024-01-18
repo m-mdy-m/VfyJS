d5d089d0d7e220e31d05732cbe5b6ca9b0f49222
mahdi <mahdimamashli1383@gmail.com>
2024-01-19 00:22:50 +0330

    adding jsdoc for IfIsNaN handle error

--- src/errors/HandleError.js
+++ src/errors/HandleError.js
@@ -169,11 +169,29 @@ exports.IfTypeMatches = (type, property, message) => {
   }
 };
 
-exports.IfIsNaN = (property,message)=>{
-  if(isNaN(property)){
-    throw TypeError(property,message)
+/**
+ * Throws a type error if the specified property is not a valid number (isNaN).
+ *
+ * This function checks if the provided property is a valid number using the isNaN function,
+ * and if not, it throws a type error with the specified property and message.
+ *
+ * @param {any} property - The property to be validated for being a valid number.
+ * @param {string} message - The error message to be associated with the type validation failure.
+ * @throws {TypeError} - Throws a type error with the specified property and message if the property is not a valid number.
+ *
+ * @example
+ * try {
+ *   IfIsNaN('abc', 'Value should be a number');
+ * } catch (error) {
+ *   console.error(error.name); // 'TypeError'
+ *   console.error(error.message); // 'Value should be a number'
+ * }
+ */
+exports.IfIsNaN = (property, message) => {
+  if (isNaN(property)) {
+    throw new TypeError(property, message);
   }
-}
+};
 
 
 /**

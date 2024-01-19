85abd7de962d3b9e77424a7ad896539a269e35fd
mahdi <mahdimamashli1383@gmail.com>
2024-01-19 12:34:14 +0330

    update jsDoc

--- src/validator/links/http.js
+++ src/validator/links/http.js
@@ -4,40 +4,53 @@ const inputValidation = require('../../utils/inputValidator');
 
 /**
  * Validates if the given URL is an HTTP URL.
+ *
  * @param {string} url - The URL to be validated.
  * @returns {boolean} Returns true if the URL is a valid HTTP URL, otherwise false.
  * @throws {TypeError} Throws a TypeError if the input is not a string.
  * @throws {Error} Throws an Error if the input is a number or if the protocol is not 'http:'.
+ *
  * @example
- * const result = isHttpUrl("http://www.example.com");
- * console.log(result); // Output: true
+ * // Example 1: Valid HTTP URL
+ * const result1 = isHttpUrl("http://www.example.com");
+ * console.log(result1); // Output: true
+ *
  * @example
+ * // Example 2: Handling an invalid HTTPS URL with try-catch
  * try {
- *   isHttpUrl("https://www.example.com");
+ *   const result2 = isHttpUrl("https://www.example.com");
+ *   console.log(result2);
  * } catch (error) {
  *   console.error(error.message); // Output: "Only HTTP URLs are allowed."
  * }
  */
 function isHttpUrl(url) {
-    const isHttp = /(?=.*(HTTP|http))/.test(url)
-    ifFalsyValue(isHttp,'The URL must contain the substring "http". Please provide a valid URL.')
+    // Check if the URL contains the substring "http"
+    const isHttp = /(?=.*(HTTP|http))/.test(url);
+    ifFalsyValue(isHttp, 'The URL must contain the substring "http". Please provide a valid URL.');
+
+    // Check if the URL is empty
     if (url === "") {
         throw new Error("URL cannot be empty. Please provide a valid URL.");
     }
+
+    // Perform type and numeric checks
     IfNotType('string', url, 'URL must be a string.');
     IfIsNumber(url, 'URL must not be a number.');
+
+    // Trim the URL
     url = trimmedValue(url);
-    
+
     /**
      * The URL object representing the validated URL.
      * @type {URL}
      */
     let { protocol, hostname, href } = new URL(url);
-    // Perform type and numeric checks
 
-    // Trim the URL
+    // Convert protocol to lowercase and trim
     protocol = protocol.toLowerCase();
     protocol = trimmedValue(protocol);
+
     /**
      * Validates if the URL uses the 'https' protocol and throws an error if it does.
      */
@@ -46,7 +59,7 @@ function isHttpUrl(url) {
     // Check HTTP format
     const hasHttp = /(HTTP:|http:)\/\/[^\/]/i.test(url);
 
-    // Validate special characters in the hostname  
+    // Validate special characters in the hostname
     const host = hostname.split('.')[1];
     const validator = inputValidation(host);
     const hasSpecial = validator.hasSpecialCharacter();

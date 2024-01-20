2eec69b4b3f9ea3ac5b3ec637edcf76104fc48a6
mahdi <mahdimamashli1383@gmail.com>
2024-01-20 16:18:54 +0330

    Update telephone format data for multiple countries in Africa.

--- src/validator/data/genTelFormats.js
+++ src/validator/data/genTelFormats.js
@@ -1,58 +1,112 @@
 const fs = require('fs')
 const path =  require('path')
 const pathFile = path.join(__dirname + '/telephone_formats.json')
-function createPhoneNumberFormat(name, code, fixed_linePattern, landLinePattern, localPattern, internationalPattern) {
-    const countryFormatObject  ={
-        "country": name,
-        "countryCode": code,
-        "formats": []
-    }
-    if (localPattern) {
-        countryFormatObject.formats.push({
-            "type": "local",
-            "pattern": localPattern,
-        });
-    }
-    if (internationalPattern) {
-        countryFormatObject.formats.push({
-            "type": "international",
-            "pattern": internationalPattern
-        });
-    }
-    if (fixed_linePattern) {
-        countryFormatObject.formats.push({
-            "type": 'fixed-line',
-            'pattern': fixed_linePattern
-        });
-    }
-    if (landLinePattern) {
-        countryFormatObject.formats.push({
-            "type": "landline",
-            "pattern": landLinePattern
-        });
+function createPhoneNumberFormat(
+continent,
+name = [],
+code = [],
+//---
+
+fixed_linePattern = [],fixPattern_name,
+landLinePattern = [],landLinePattern_name,
+localPattern = [],localPattern_name,
+internationalPattern = [],fixPattern_international,{objOption}={})
+ {
+    const countries = [];
+
+    for (let i = 0; i < name.length; i++) {
+        const countryFormatObject = {
+            "continent": continent,
+            "country": name[i],
+            "countryCode": code[i],
+            "formats": []
+        };
+
+        if (localPattern && localPattern[i]) {
+            countryFormatObject.formats.push({
+                "type": localPattern_name,
+                "pattern": localPattern[i],
+            });
+        }
+        if (internationalPattern && internationalPattern[i]) {
+            countryFormatObject.formats.push({
+                "type": fixPattern_international,
+                "pattern": internationalPattern[i]
+            });
+        }
+        if (fixed_linePattern && fixed_linePattern[i]) {
+            countryFormatObject.formats.push({
+                "type": fixPattern_name,
+                'pattern': fixed_linePattern[i]
+            });
+        }
+        if (landLinePattern && landLinePattern[i]) {
+            countryFormatObject.formats.push({
+                "type": landLinePattern_name,
+                "pattern": landLinePattern[i]
+            });
+        }
+
+        countries.push(countryFormatObject);
     }
 
-    return countryFormatObject;
+    return countries;
 }
 
 function writeToJsonFile(data) {
-    let existsCountry = data.country
-    let countryExists = false
+    let existsCountries = data.map(country => country.country);
+    let countryExists = false;
     let existingData = [];
+
     if (fs.existsSync(pathFile)) {
-        const fileContent = fs.readFileSync(pathFile,'utf8')
-        existingData = JSON.parse(fileContent)
-        countryExists = existingData.some(content => content.country === existsCountry )
+        const fileContent = fs.readFileSync(pathFile, 'utf8');
+        existingData = JSON.parse(fileContent);
+
+        // Check if any country already exists in the file
+        countryExists = existingData.some(content => existsCountries.includes(content.country));
+
         if (countryExists) {
-            console.log(`Country "${existsCountry}" already exists.`);
-        }else{
-            existingData.push(data)
-            fs.writeFileSync(pathFile,JSON.stringify(existingData),'utf8')
+            console.log(`Countries already exist in the JSON file.`);
+        } else {
+            // Add the new countries data to the existing data
+            existingData.push(...data);
+            fs.writeFileSync(pathFile, JSON.stringify(existingData, null, 2), 'utf8');
+            console.log(`Countries added to the JSON file.`);
         }
-    }else{
-        existingData.push(data)
-        fs.writeFileSync(pathFile,JSON.stringify(existingData),'utf8')
+    } else {
+        // If the file doesn't exist, create it with the new data
+        existingData.push(...data);
+        fs.writeFileSync(pathFile, JSON.stringify(existingData, null, 2), 'utf8');
+        console.log(`Countries added to the JSON file.`);
     }
 }
-const formatObject = createPhoneNumberFormat("iran", "253", "^(21|27)[0-9]{6}$", null, "77[0-9]{6}$", "^\\+253[0-9]{8}$");
-writeToJsonFile(formatObject);
+// information for AFrica
+const Africa = 'Africa'
+const countryAfrica = ['Djibouti','Morocco','Kenya','South Africa']
+const codeCountryAfrica = ['"253"','212','254','27']
+const localPatternAfrica = ["^(21|27)[0-9]{4}$","^(05[0-9]{2}){2}[0-9]{4}$","^0[0-9]{3}[0-9]{6}$","^(0[0-9]{2}|0AA)[0-9]{7}$"]
+const localPatternAfrica_name = 'local'
+const internationalPatternAfrica = ["^\\+253[0-9]{8}$","^\\+212[567][0-9]{8}$","^\\+254[0-9]{9}$","^\\+27[0-9]{9}$"]
+const internationalPatternAfrica_name = 'international'
+const fixed_linePatternAfrica = ["^(21|27)[0-9]{4}$","^(05[0-9]{2}){2}[0-9]{4}$","^0[0-9]{3}[0-9]{6}$", "^(0[0-9]{2}|0AA)[0-9]{7}$"]
+const fixed_linePatternAfrica_name = 'fixed-line'
+const formatObjectAfrica = 
+createPhoneNumberFormat
+(
+    Africa,
+    countryAfrica
+    ,codeCountryAfrica
+    ,fixed_linePatternAfrica , fixed_linePatternAfrica_name
+    ,null,null
+    ,localPatternAfrica,localPatternAfrica_name
+    ,internationalPatternAfrica,internationalPatternAfrica_name
+    );
+writeToJsonFile(formatObjectAfrica);
+/// information  Asia
+const Asia = 'Asia'
+const countryAsia = []
+const codeCountryAsia = []
+const localPattern = []
+const internationalPattern = []
+const fixed_linePattern = []
+

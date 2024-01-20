577a99d1c271d59f2ea106f3a7a99e7919711aaa
mahdi <mahdimamashli1383@gmail.com>
2024-01-20 15:29:07 +0330

    Add Iran telephone number formats to telephone_formats.json

--- src/validator/data/genTelFormats.js
+++ src/validator/data/genTelFormats.js
@@ -1,17 +1,12 @@
 const fs = require('fs')
 const path =  require('path')
 const pathFile = path.join(__dirname + '/telephone_formats.json')
-console.log('path =>',pathFile);
 function createPhoneNumberFormat(name, code, fixed_linePattern, landLinePattern, localPattern, internationalPattern) {
-    let countryFormatObject  = {}
-    const countryFormatsArray  =[{
+    const countryFormatObject  ={
         "country": name,
         "countryCode": code,
         "formats": []
-    }]
-    countryFormatsArray .forEach(format =>{
-        countryFormatObject = format
-    })
+    }
     if (localPattern) {
         countryFormatObject.formats.push({
             "type": "local",
@@ -37,43 +32,27 @@ function createPhoneNumberFormat(name, code, fixed_linePattern, landLinePattern,
         });
     }
 
-    return countryFormatsArray;
+    return countryFormatObject;
 }
 
 function writeToJsonFile(data) {
-    // fs.writeFileSync(pathFile, JSON.stringify(data))
-    // const dataCountry = data
-    // console.log(dataCountry);
-    // const jsonData = fs.readFileSync(pathFile,'utf8')
-    // const countries = JSON.parse(jsonData)
-    
-    // const countryExists  = ''
-    // // let existingData = [];
-
-    // // Check if the file exists
-    // if (fs.existsSync(pathFile)) {
-    //     // Read existing data from the file
-    //     const fileContent = fs.readFileSync(pathFile, 'utf8');
-    //     console.log('fileContent =>', fileContent);
-
-    //     // Parse existing data as JSON if the file is not empty
-    //     if (fileContent.trim() !== '') {
-    //         existingData = JSON.parse(fileContent);
-    //         console.log('existingData (parsed) =>', existingData);
-
-    //         // Ensure existingData is an array
-    //         if (!Array.isArray(existingData)) {
-    //             existingData = [existingData];
-    //         }
-    //     }
-    // }
-
-    // // Add new data to existing data
-    // existingData.push(data);
-    // console.log('mergedData =>', existingData);
-
-    // // Write merged data back to the file
-    // fs.writeFileSync(pathFile, JSON.stringify(existingData, null, 2));
+    let existsCountry = data.country
+    let countryExists = false
+    let existingData = [];
+    if (fs.existsSync(pathFile)) {
+        const fileContent = fs.readFileSync(pathFile,'utf8')
+        existingData = JSON.parse(fileContent)
+        countryExists = existingData.some(content => content.country === existsCountry )
+        if (countryExists) {
+            console.log(`Country "${existsCountry}" already exists.`);
+        }else{
+            existingData.push(data)
+            fs.writeFileSync(pathFile,JSON.stringify(existingData),'utf8')
+        }
+    }else{
+        existingData.push(data)
+        fs.writeFileSync(pathFile,JSON.stringify(existingData),'utf8')
+    }
 }
-const formatObject = createPhoneNumberFormat("Djibouti", "253", "^(21|27)[0-9]{6}$", null, "77[0-9]{6}$", "^\\+253[0-9]{8}$");
+const formatObject = createPhoneNumberFormat("iran", "253", "^(21|27)[0-9]{6}$", null, "77[0-9]{6}$", "^\\+253[0-9]{8}$");
 writeToJsonFile(formatObject);

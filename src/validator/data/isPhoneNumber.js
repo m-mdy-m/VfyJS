44bf36ff80bf8635ef537a2929e2eb28be6dad35
mahdi <mahdimamashli1383@gmail.com>
2024-01-20 14:58:35 +0330

    update function read phone number

--- src/validator/data/isPhoneNumber.js
+++ src/validator/data/isPhoneNumber.js
@@ -1,10 +1,15 @@
-
-function phoneNumber(code,phone){
-    return code + phone
-
-
+const readPhoneCodeData = require('./getPhoneCode')
+async function phoneNumber(code,phone){
+    const getPhoneData = await readPhoneCodeData()
+    const countries = getPhoneData.countries
+    const isoCodes = getPhoneData.isoCodes
+    const phoneCodes = getPhoneData.phoneCodes
+    const indexOfCode = phoneCodes.indexOf(code)
+    const result = phoneCodes[indexOfCode]
 }
 const result = phoneNumber('98',"915291407")
+// [+][country code][area code][local phone number]
+//  + 1 415 123 1234
 
 console.log('result =>', result);
 module.exports = phoneNumber

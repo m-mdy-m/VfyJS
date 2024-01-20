5b5eea04779d98dfa0071179abaddc6028e793cf
mahdi <mahdimamashli1383@gmail.com>
2024-01-20 17:54:48 +0330

    Update telephone formats for Asia, Oceania, and Europe

--- src/validator/data/genTelFormats.js
+++ src/validator/data/genTelFormats.js
@@ -120,3 +120,151 @@ const asianInternationalPatterns = ['^\\d{3,5}$',null,"^\\d{3,4}$","^\\+98 (0\\d
 const asianInternationalPatternNam = 'service'
 const asianFormatObject = createPhoneNumberFormats(asiaContinent,asianCountries,asianCountryCodes,asianFixedLinePatterns,asianFixedLinePatternName,asianLandLinePatterns,asianLandLinePatternName,asianLocalPatterns,asianLocalPatternName,asianInternationalPatterns,asianInternationalPatternNam)
 writeToJsonFile(asianFormatObject)
+
+
+// Information for Oceania
+const oceaniaContinent = 'Oceania';
+const oceaniaCountries = ['Australia', 'New Zealand'];
+const oceaniaCountryCodes = ['61', '64'];
+
+// Patterns for Australia
+const oceaniaFixedLinePatternsAustralia = ['^(0[2378])?\\d{8}$', '^\\d{4} \\d{4}$', "^\\d{3,4}-\\d{6,7}$", "^0\\d{2} \\d{8}$", "^0\\d{2}-\\d{4}-\\d{4}$", "^0\\d{1,2}-\\d{7,8}$", "^0\\d{2}-\\d{7,8}$", "^\\d{11}$", "^\\(\\d{3}\\) \\d{1} \\d{6}$", "^0\\d{1,3}-\\d{3,4}-\\d{4}$", "^\\(\\d{2}\\) \\d{4} \\d{4}$", "^\\d{4} \\d{4}$"];
+const oceaniaFixedLinePatternNameAustralia = 'landline';
+const oceaniaLocalPatternsAustralia = ['^1[3-9]\\d{9}$', null, "^9[1-9]\\d{9}$", "^09\\d{9}$", "^(0120|0570|0800)-\\d{2}-\\d{4}$", "^01\\d{1}-\\d{6,8}$", null, "^(\\+63|0)\\d{10}$", "^\\d{3} \\d{7}$", "^01\\d{1}-\\d{4}-\\d{4}$", "^0 \\d{3} \\d{4}$"];
+const oceaniaLocalPatternNameAustralia = 'mobile';
+const oceaniaLandLinePatternsAustralia = ['^(800|400) \\d{4} \\d{4}$', null, "^1800 \\d{6}$", null, null, "^1-800-\\d{2}-\\d{4}$", null, null, null, "^070-\\d{4}-\\d{4}$", "^09\\d{2} \\d{6}$", null];
+const oceaniaLandLinePatternNameAustralia = 'toll-free';
+const oceaniaInternationalPatternsAustralia = ['^\\d{3,5}$', null, "^\\d{3,4}$", "^\\+61\\d{8}$", "^1[12]$", "^19\\d{6}$"];
+const oceaniaInternationalPatternNameAustralia = 'service';
+const oceaniaFormatObject = createPhoneNumberFormats(
+    oceaniaContinent,
+    oceaniaCountries,
+    oceaniaCountryCodes,
+    oceaniaFixedLinePatternsAustralia,
+    oceaniaFixedLinePatternNameAustralia,
+    oceaniaLandLinePatternsAustralia,
+    oceaniaLandLinePatternNameAustralia,
+    oceaniaLocalPatternsAustralia,
+    oceaniaLocalPatternNameAustralia,
+    oceaniaInternationalPatternsAustralia,
+    oceaniaInternationalPatternNameAustralia
+);
+
+writeToJsonFile(oceaniaFormatObject);
+
+
+// Information for Europe
+const europeContinent = 'Europe';
+const europeanCountries = ['Belgium', 'Denmark', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia'];
+const europeanCountryCodes = ['32', '45', '358', '33', '49', '30', '36', '354', '353', '39', '31', '47', '48', '351', '40', '7'];
+
+const europeanFixedLinePatterns = [
+  '^0[0-9]{1}[0-9]{1} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  '^[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  '^0[0-9]{1}-[0-9]{3} [0-9]{3} [0-9]{2}$',
+  '^0[1-9]{1} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  '^0[0-9]{3} [0-9]{6,8}(-[0-9]{2})?$',
+  '^[0-9]{3} [0-9]{7,8}$',
+  '^[0-9]{2} [0-9]{6}$',
+  '^[0-9]{3} [0-9]{4}$',
+  '^[0-9]{2,3} [0-9]{5,7}$',
+  'Variable length, no fixed convention',
+  '^0[0-9]{1,2}-[0-9]{7,9}$',
+  '^[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  '^[0-9]{3}-[0-9]{3}-[0-9]{3}$',
+  '^[0-9]{3} [0-9]{3} [0-9]{3}$',
+  '^[0-9]{4}-[0-9]{3}-[0-9]{3}$',
+  '^[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}$',
+  '^[0-9]{3}-[0-9]{2}-[0-9]{2}$',
+  '^[+]{1}7 [(]{1}[0-9]{3}[)]{1} [0-9]{3}-[0-9]{2}-[0-9]{2}$'
+];
+
+const europeanFixedLinePatternName = 'landline';
+
+const europeanLocalPatterns = [
+  '^04[0-9]{1} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  null,
+  '^0[7-9]{1}[0-9]{1}-[0-9]{3} [0-9]{2} [0-9]{2}$',
+  '^06 [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  '^[0-9]{4}$',
+  '^08[0-9]{1} [0-9]{6}$',
+  '^(1800|1850|1550) [0-9]{3} [0-9]{3}$',
+  'Variable length, no fixed convention',
+  '^06-[0-9]{8}$',
+  '^14 [0-9]{2}$',
+  '^[+]{1}31 [0-9]{2} [0-9]{7,9}$',
+  '^[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  '^[0-9]{3}-[0-9]{3}-[0-9]{3}$',
+  '^[0-9]{3} [0-9]{3} [0-9]{3}$',
+  '^[0-9]{4}-[0-9]{3}-[0-9]{3}$',
+  '^[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}$',
+  '^[0-9]{3}-[0-9]{2}-[0-9]{2}$',
+  '^[+]{1}7 [(]{1}[0-9]{3}[)]{1} [0-9]{3}-[0-9]{2}-[0-9]{2}$'
+];
+
+const europeanLocalPatternName = 'mobile';
+
+const europeanLandLinePatterns = [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null
+];
+
+const europeanLandLinePatternName = 'toll-free';
+
+const europeanInternationalPatterns = [
+  null,
+  null,
+  null,
+  null,
+  '^[+]{1}49 [0-9]{4} [0-9]{6,8}$',
+  '^[+]{1}30 [0-9]{3} [0-9]{7,8}$',
+  null,
+  null,
+  '^[0-9]{3} [0-9]{4}$',
+  '^[+]{1}353 [0-9]{2} [0-9]{5,7}$',
+  'Variable length, no fixed convention',
+  '^[+]{1}31 [0-9]{2} [0-9]{7,9}$',
+  '^[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$',
+  '^[0-9]{3}-[0-9]{3}-[0-9]{3}$',
+  '^[0-9]{3} [0-9]{3} [0-9]{3}$',
+  '^[0-9]{4}-[0-9]{3}-[0-9]{3}$',
+  '^[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}$',
+  '^[0-9]{3}-[0-9]{2}-[0-9]{2}$',
+  '^[+]{1}7 [(]{1}[0-9]{3}[)]{1} [0-9]{3}-[0-9]{2}-[0-9]{2}$'
+];
+
+const europeanInternationalPatternName = 'service';
+
+const europeanFormatObject = createPhoneNumberFormats(
+  europeContinent,
+  europeanCountries,
+  europeanCountryCodes,
+  europeanFixedLinePatterns,
+  europeanFixedLinePatternName,
+  europeanLandLinePatterns,
+  europeanLandLinePatternName,
+  europeanLocalPatterns,
+  europeanLocalPatternName,
+  europeanInternationalPatterns,
+  europeanInternationalPatternName
+);
+
+writeToJsonFile(europeanFormatObject);

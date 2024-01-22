530cc30b3eba3c51926b55feabb95f2ee0bc7bc0
mahdi <mahdimamashli1383@gmail.com>
2024-01-22 17:45:11 +0330

    Format validation for Asian phone numbers. and functionality  codes

--- src/validator/phone/countries/Asian.js
+++ src/validator/phone/countries/Asian.js
@@ -1,24 +1,14 @@
 const {generateValidationResult , validationFormats,extractInfoValue} = require('../utils/FormatValidation')
 exports.iran = (values) => {
     const { code, phone, patterns,hasCode,hasPhone } = extractInfoValue(values)
-    let mobileFormat = `0${phone}`;
-    let serviceFormat = `+${code}0${phone}`;
-    let landline = `0${phone}`;
-    const hasValidFormat = validationFormats(patterns,mobileFormat,landline,serviceFormat)
-    if (hasValidFormat.every((result) => result)) {
-      return generateValidationResult(values,hasValidFormat,hasCode,hasPhone,false)
-    } else {
-
-      return generateValidationResult(values,hasValidFormat,hasCode,hasPhone,false);
-    }
+    const format = [ `0${phone}`,`+${code}0${phone}`, `0${phone}`]
+    const hasValidFormat = validationFormats(patterns,format)
+    return generateValidationResult(values,hasValidFormat,hasCode,hasPhone,false);
   };
 
 exports.China = (values)=>{
-    const { code,hasCode,patterns,hasPhone,phone ,default:value} = extractInfoValue(values)
-    const hasOne = /(?=.*[1])/.test(phone)
-    console.log(phone);
-    const mobile = `${phone}`;
-    const service = `+${code}0${phone}`;
-    const landline = `0${phone}`;
-    const tollFree = ''
+    const { hasCode,patterns,hasPhone,phone} = extractInfoValue(values)
+    const format = [phone, phone, `0${phone}`, phone];
+    const hasValidFormat =validationFormats(patterns,format)
+    return generateValidationResult(values,hasValidFormat,hasCode,hasPhone,false)
 }

ed25bfacd2cf274ba6e7f42c16108d7bf3666c8a
mahdi <mahdimamashli1383@gmail.com>
2024-01-22 16:37:13 +0330

    adding countries Asia

--- src/validator/phone/continents/Asia.js
+++ src/validator/phone/continents/Asia.js
@@ -1,10 +1,32 @@
-const {iran} = require('../countries/Asian')
+const {iran,China} = require('../countries/Asian')
 function AsiaPhoneNumber(value){
     const country = value.country
     switch (country){
+        case "China":
+            return China(value)
+        case "Hong Kong and Macau":
+            return '//'
+        case "India":
+            return '//'
         case "Iran":
             return iran(value)
-        case "":
+        case "Japan":
+            return '//'
+        case "Malaysia":
+            return '//'
+        case "Pakistan":
+            return '//'
+        case "Philippines":
+            return '//'
+        case "Singapore":
+            return '//'
+        case "Sri Lanka":
+            return '//'
+        case "South Korea":
+            return '//'
+        case "Taiwan":
+            return '//'
+        case "Thailand":
             return '//'
     }
 }

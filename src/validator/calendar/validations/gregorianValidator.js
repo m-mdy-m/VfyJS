bc525b42cf4f0b4025069cc81176bd9459ff4d21
mahdi <mahdimamashli1383@gmail.com>
2024-01-25 16:44:22 +0330

    Update Gregorian date validation to handle special characters and alphabetic characters.

--- src/validator/calendar/validations/gregorianValidator.js
+++ src/validator/calendar/validations/gregorianValidator.js
@@ -2,25 +2,40 @@ const {getSubstring}  = require('../../phone/utils/FormatValidation')
 const {trimmedValue} = require('../../../common/validationConstants')
 const inputValidator= require('../../../utils/inputValidator')
 const { ifTruthyValue, TypesCheck }= require('../../../errors/HandleError')
-function validateGregorianDate(year, month, day) {
-    TypesCheck((year || month || day), ['number','string'],'Year, month, and day must be either numbers or strings.')
-    const Dates = [year,month,day]
-    Dates.forEach(date =>{
-        let hasSpecialChar = inputValidator(date).hasSpecialCharacter()
-        ifTruthyValue((hasSpecialChar),'Year, month, or day cannot contain special characters.')
+function validateGregorianDate(inputYear, inputMonth, inputDay) {
+    // Create an array to hold the input values (year, month, day)
+    const dateComponents = [inputYear, inputMonth, inputDay];
+
+    // Iterate over each date component (year, month, day)
+    dateComponents.forEach((component, index) => {
+        // Check if the component is of type 'number' or 'string'
+        TypesCheck(component, ['number', 'string'], 'Year, month, and day must be either numbers or strings.');
+
+        // Check for the presence of special characters in the date component
+        const validator = inputValidator(component);
+        const hasSpecialChar = validator.hasSpecialCharacter();
+        const hasAlphabetic = validator.hasAlphabetic();
+
+        // Display error message if special characters are found
+        ifTruthyValue(hasSpecialChar, 'Year, month, or day cannot contain special characters.');
+
+        // Display error message if alphabetic characters are found
+        ifTruthyValue(hasAlphabetic, 'The value cannot contain alphabetic characters.');
+
+        // Convert the date component to a string
+        dateComponents[index] = component.toString();
+
+        // Update the date component with the trimmed string representation
+        dateComponents[index] = trimmedValue(dateComponents[index]);
     })
-    Dates.forEach((date, index) => {
-        Dates[index] = date.toString();
-        Dates[index] =trimmedValue(date[index])
-    });
+    // Get the current date
     const nowDate = new Date();
     const nowYear = nowDate.getFullYear();
-    const nowMonth = nowDate.getMonth() + 1; 
+    const nowMonth = nowDate.getMonth() + 1;
     const nowDay = nowDate.getDate();
-    console.log();
 }
 
 const year = 2024;
 const month = 12;
 const day = 1;
-console.log(validateGregorianDate('2  0  2    4', 12 , day)); 
+console.log(validateGregorianDate('2  0 a 2    4', 12 , day)); 

e2e0ef96d1ba4781d19494a280355e62201c8579
mahdi <mahdimamashli1383@gmail.com>
2024-01-29 01:44:07 +0330

    update config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production', 
+  mode: 'production',
   entry: './index.js',
   output: {
     filename: 'vfyjs.browser.js',
     path: path.resolve(__dirname),
     library: 'vfyjs',
     libraryTarget: 'umd',
+    globalObject: 'this',
   },
   resolve: {
     fallback: {
-      fs: false,
       path: require.resolve('path-browserify'),
+      fs: false, 
+    },
+  },
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
   },
 };

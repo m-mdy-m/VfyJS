1f1d051849f2f39fba4725623b8f35c77c3adf54
mahdi <mahdimamashli1383@gmail.com>
2024-02-05 00:57:00 +0330

    Added  feature binary search

---
+++ src/validator/form/BinaryTree/SearchUsername.js
@@ -0,0 +1,50 @@
+class TreeNode {
+  constructor(value) {
+    this.root = value;
+    this.left = null;
+    this.right = null;
+  }
+  addNode(node, value) {
+    if (value < node.value) {
+      if (!this.left) {
+        this.left = node;
+      } else {
+        this.left.addNode(node.left, value);
+      }
+    } else if (value > node.value) {
+      if (!this.right) {
+        this.right = node;
+      } else {
+        this.right.addNode(node.right, value);
+      }
+    }
+  }
+  searchTree(node,value){
+    if (!node) {
+        return false
+    }
+    if (value === node.value) {
+        return false
+    }else if(value <  node.value && this.left!==null) {
+        return node.left.searchTree(node.left,value)
+    }else if(value < node.value && this.right!==null){
+        return node.right.searchTree(node.right,value)
+    }
+    return null
+  }
+}
+class SearchBinary {
+  constructor(value) {
+    this.root = null;
+  }
+  insert(value) {
+    if (!this.root) {
+      this.root = new TreeNode(value);
+    } else {
+      this.root.addNode(this.root, value);
+    }
+  }
+  search(value){
+    return this.searchTree(this.root,value)
+  }
+}

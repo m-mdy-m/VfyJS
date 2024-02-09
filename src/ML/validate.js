5d99e1b6cc51ee7c2d1732f85485e04d586aa1c5
mahdi <mahdimamashli1383@gmail.com>
2024-02-10 02:54:20 +0330

    Changed the class name from `validate` to `Validate`. Changed the constructor to accept `trainingData` instead of `modelMl`. Created a new instance of `TrainModel` within the constructor using the provided `trainingData`. Updated the logic to calculate the predicted value using the slope and intercept of the trained model. Updated the example usage to use the `trainingData` and `inputData` variables.

--- src/ML/validate.js
+++ src/ML/validate.js
@@ -1,33 +1,38 @@
 const TrainModel = require("./model");
-class validate {
-  constructor(rule, modelMl) {
-    this.result = {}
+class Validate {
+  constructor(rule, trainingData) {
+    this.result = {};
     this.rule = rule;
+
+    const trainModel = new TrainModel(trainingData);
+
     for (const key in rule) {
-        if (Object.hasOwnProperty.call(rule, key)) {
-            this.obj = rule[key];
-            this.trainModel = new TrainModel(modelMl)
-            this.value = this.trainModel.model.slope * this.obj + this.trainModel.model.intercept
-            if (this.value >= 0 && this.value <= 100) {
-                this.result[key] = true; // Valid
-              } else {
-                this.result[key] = false; // Invalid
-              }
+      if (rule.hasOwnProperty(key)) {
+        const inputValue = rule[key];
+        const predictedValue =
+          trainModel.model.slope * inputValue + trainModel.model.intercept;
+
+        if (predictedValue >= 0 && predictedValue <= 100) {
+          this.result[key] = true; // Valid
+        } else {
+          this.result[key] = false; // Invalid
         }
+      }
     }
-    return this.result
   }
 }
-
-const input = [
+// Example usage:
+const trainingData = [
   { x: 1, y: 20 },
   { x: 2, y: 40 },
   { x: 3, y: 60 },
   { x: 4, y: 80 },
 ];
+
 const inputData = {
-    feature1: 4,
-    feature2: 40
-  };
-const result = new validate(inputData,input)
-console.log('result =>',result);
+  feature1: 4,
+  feature2: 40,
+};
+
+const result = new Validate(inputData, trainingData);
+console.log("result =>", result);

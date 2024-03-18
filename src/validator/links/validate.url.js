872fe64f9277018ce7c699b8c43e6eafe3f38657
mahdi <mahdimamashli1383@gmail.com>
2024-03-18 13:58:08 +0330

    Refactor code to use `validateUrl` function for `isHttp` and `isHttps` functions.

--- src/validator/links/validate.url.js
+++ src/validator/links/validate.url.js
@@ -71,5 +71,55 @@ function validateUrl(url, expectedProtocol) {
   // Return true if the URL has the expected format
   return hasCorrectFormat;
 }
+/**
+ * Validates if the given URL is an HTTPS URL.
+ *
+ * @param {string} url - The URL to be validated.
+ * @returns {boolean} Returns true if the URL is a valid HTTPS URL, otherwise false.
+ * @throws {TypeError} Throws a TypeError if the input is not a string.
+ * @throws {Error} Throws an Error if the input is a number or if the protocol is not 'https:'.
+ *
+ * @example
+ * // Example 1: Valid HTTPS URL
+ * const result1 = isHttps("https://www.example.com");
+ * console.log(result1); // Output: true
+ *
+ * @example
+ * // Example 2: Handling an invalid HTTP URL with try-catch
+ * try {
+ *   const result2 = isHttps("http://www.example.com");
+ *   console.log(result2);
+ * } catch (error) {
+ *   console.error(error.message); // Output: "Only HTTPS URLs are allowed."
+ * }
+ */
+function isHttpsUrl(url) {
+    return validateUrl(url,'https')
+}
 
-module.exports = validateUrl;
+/**
+ * Validates if the given URL is an HTTP URL.
+ *
+ * @param {string} url - The URL to be validated.
+ * @returns {boolean} Returns true if the URL is a valid HTTP URL, otherwise false.
+ * @throws {TypeError} Throws a TypeError if the input is not a string.
+ * @throws {Error} Throws an Error if the input is a number or if the protocol is not 'http:'.
+ *
+ * @example
+ * // Example 1: Valid HTTP URL
+ * const result1 = isHttp("http://www.example.com");
+ * console.log(result1); // Output: true
+ *
+ * @example
+ * // Example 2: Handling an invalid HTTPS URL with try-catch
+ * try {
+ *   const result2 = isHttp("https://www.example.com");
+ *   console.log(result2);
+ * } catch (error) {
+ *   console.error(error.message); // Output: "Only HTTP URLs are allowed."
+ * }
+ */
+function isHttp(url) {
+    return validateUrl(url,'http');
+}
+module.exports = {isHttp,isHttps : isHttpsUrl};

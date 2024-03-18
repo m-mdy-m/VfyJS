1ddbe0746942a5384ec35a93bd6466846e00107b
mahdi <mahdimamashli1383@gmail.com>
2024-03-18 04:21:21 +0330

    Refactor validation logic using classes and interfaces

--- src/validator/requests/body.js
+++ src/validator/requests/body.js
@@ -1,88 +1,86 @@
 class ValidationBody {
-  constructor(req) {
-    this._body = req.body;
-  }
-
-  validate(rules) {
-    const errors = {};
-    for (const field in rules) {
-      const fieldRules = rules[field].split("|");
-      for (const rule of fieldRules) {
-        const [ruleName, ruleValue] = rule.split(":");
-        const validationMethod = this.getValidationMethod(ruleName);
-        if (validationMethod) {
-          const error = validationMethod(field, ruleValue);
+    constructor(req) {
+      this._body = req.body;
+      this.validators = {
+        required: new RequiredValidator(),
+        string: new StringTypeValidator(),
+        number: new NumberTypeValidator(),
+        min: new MinLengthValidator(),
+        max: new MaxLengthValidator(),
+      };
+    }
+  
+    validate(rules) {
+      const errors = {};
+      for (const field in rules) {
+        const fieldRules = rules[field].split("|");
+        for (const rule of fieldRules) {
+          const [ruleName, ruleValue] = rule.split(":");
+          const validator = this.validators[ruleName];
+          if (!validator) {
+            throw new Error(`Validation rule '${ruleName}' is not supported.`);
+          }
+          const error = validator.validate(field, ruleValue, this._body);
           if (error) {
             errors[field] = error;
             break; // Stop further validation for this field if error found
           }
-        } else {
-          throw new Error(`Validation rule '${ruleName}' is not supported.`);
         }
       }
-    }
-    return errors;
-  }
-
-  getValidationMethod(ruleName) {
-    switch (ruleName) {
-      case "required":
-        return this.validateRequired;
-      case "string":
-        return this.validateString;
-      case "number":
-        return this.validateNumber;
-      case "min":
-        return this.validateMin;
-      case "max":
-        return this.validateMax;
-      default:
-        return null;
+      return errors;
     }
   }
-
-  validateRequired(field, ruleValue) {
-    if (!this._body[field]) {
-      return `${field} is required.`;
+  
+  class Validator {
+    validate(field, ruleValue, body) {
+      throw new Error("Method not implemented.");
     }
-    return null;
   }
-
-  validateString(field) {
-    if (typeof this._body[field] !== "string") {
-      return `${field} must be a string.`;
+  
+  class RequiredValidator extends Validator {
+    validate(field, ruleValue, body) {
+      if (!body[field]) {
+        return `${field} is required.`;
+      }
+      return null;
     }
-    return null;
   }
-
-  validateNumber(field) {
-    if (typeof this._body[field] !== "number") {
-      return `${field} must be a number.`;
+  
+  class StringTypeValidator extends Validator {
+    validate(field, ruleValue, body) {
+      if (typeof body[field] !== "string") {
+        return `${field} must be a string.`;
+      }
+      return null;
     }
-    return null;
   }
-
-  validateMin(field, ruleValue) {
-    if (this._body[field].length < parseInt(ruleValue)) {
-      return `${field} must be at least ${ruleValue} characters long.`;
+  
+  class NumberTypeValidator extends Validator {
+    validate(field, ruleValue, body) {
+      if (typeof body[field] !== "number") {
+        return `${field} must be a number.`;
+      }
+      return null;
     }
-    return null;
   }
-
-  validateMax(field, ruleValue) {
-    if (this._body[field].length > parseInt(ruleValue)) {
-      return `${field} cannot exceed ${ruleValue} characters.`;
+  
+  class MinLengthValidator extends Validator {
+    validate(field, ruleValue, body) {
+      if (body[field].length < parseInt(ruleValue)) {
+        return `${field} must be at least ${ruleValue} characters long.`;
+      }
+      return null;
     }
-    return null;
   }
-}
-
-module.exports = ValidationBody;
-
-router.get("/", (req, res, nxt) => {
-  const result = validation(req.body, {
-    username: "required|string|min:2|max:10",
-    password: "required|number|min:8|max:20|",
-    email: "required",
-  });
-});
+  
+  class MaxLengthValidator extends Validator {
+    validate(field, ruleValue, body) {
+      if (body[field].length > parseInt(ruleValue)) {
+        return `${field} cannot exceed ${ruleValue} characters.`;
+      }
+      return null;
+    }
+  }
+  
+  module.exports = ValidationBody;
+  

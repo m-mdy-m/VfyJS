ea9259adb0d14bcfe7ad7cd0b46b3d5dbd4d5b68
mahdi <mahdimamashli1383@gmail.com>
2024-03-18 12:41:13 +0330

    Update validator to use class methods

--- src/validator/requests/Validator.js
+++ src/validator/requests/Validator.js
@@ -1,6 +1,19 @@
+const inputValidator = require("../../utils/inputValidator");
+
 class Validator {
-  validate(field, ruleValue, body) {
+  constructor(field) {
+    this.field = field;
+    this.validator = inputValidator(field);
+  }
+  validate() {
     throw new Error("Method not implemented.");
+  }
+  isAlpha(value) {
+    return this.validator.hasAlphanumeric(value);
+  }
+
+  isNumeric(value) {
+    return this.validator.hasNumeric(value);
   }
 }
 
--- src/validator/requests/Validators/Type.js
+++ src/validator/requests/Validators/Type.js
@@ -1,12 +1,10 @@
-const inputValidator = require("../../../utils/inputValidator");
 const Validator = require("../Validator");
 class StringTypeValidator extends Validator {
   validate(field, ruleValue, body) {
     if (typeof body[field] !== "string") {
       return `${field} must be a string.`;
     }
-    const validator = inputValidator(field);
-    if (!validator.hasAlphanumeric(body[field])) {
+    if (!this.isAlpha(body[field])) {
       return `${field} must contain only alphanumeric characters.`;
     }
 
@@ -21,8 +19,7 @@ class NumberTypeValidator extends Validator {
     }
     // Convert the number to a string for alphanumeric validation
     const valueAsString = body[field].toString();
-    const validator = inputValidator(field);
-    if (!validator.hasNumeric(valueAsString)) {
+    if (!this.isNumeric(valueAsString)) {
       return `${field} must contain only numeric characters.`;
     }
     return null;

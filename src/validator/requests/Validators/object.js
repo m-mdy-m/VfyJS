5b18c15e9b644a8d6784a0e3611fe65301ad4bdd
mahdi <mahdimamashli1383@gmail.com>
2024-03-19 11:16:48 +0330

    Update object key validator to handle exceptions

--- src/validator/requests/Validators/object.js
+++ src/validator/requests/Validators/object.js
@@ -2,18 +2,24 @@ const Validator = require("../Validator");
 
 class ObjectKeyValidator extends Validator {
   validate(field, ruleValue, body) {
-    const expectedKeys = ruleValue.split(",");
-    const actualKeys = Object.keys(body[field]);
+    try {
+      const expectedKeys = ruleValue.split(",");
+      const actualKeys = Object.keys(body[field]);
 
-    // Find missing keys
-    const missingKeys = expectedKeys.filter((key) => !actualKeys.includes(key));
+      // Find missing keys
+      const missingKeys = expectedKeys.filter(
+        (key) => !actualKeys.includes(key)
+      );
 
-    // If missing keys are found, return an error message
-    if (missingKeys.length > 0) {
-      return `Missing keys in '${field}': ${missingKeys.join(", ")}.`;
+      // If missing keys are found, return an error message
+      if (missingKeys.length > 0) {
+        return `Missing keys in '${field}': ${missingKeys.join(", ")}.`;
+      }
+
+      return null; // Return null if all keys are present
+    } catch (error) {
+      return error.message; // Return error message if an exception occurs
     }
-
-    return null; // Return null if all keys are present
   }
 }
 

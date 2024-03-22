cce928168d32517b3abe8d29ea6ebd2b17cf7c26
mahdi <mahdimamashli1383@gmail.com>
2024-03-22 05:00:49 +0330

    add comments for config options email password email and change logic

--- src/validator/form/helper/config.js
+++ src/validator/form/helper/config.js
@@ -1,35 +1,54 @@
-const  { defaultOptions }  = require ( "../global.config")
-const  createValidationOptions  = require ( "./genOptions")
+const { defaultOptions } = require("../global.config");
+const createValidationOptions = require("./genOptions");
 
+/**
+ * Generate validation options for username based on provided options and default settings.
+ * @param {Object} options - Options for customizing username validation criteria.
+ * @returns {Object} - Object containing validation options for username.
+ */
+function optionUsername(options) {
+    // Extract default settings for username validation
+    const { alphanumeric, maxLength, minLength, whitespace } = defaultOptions.Username;
 
-function optionUsername(options){
-    const { alphanumeric,maxLength,minLength,whitespace } = defaultOptions.Username
-    const names = [
-        'alphanumeric',
-        'maxLength',
-        'minLength',
-        'whitespace',
-    ]
+    // Define names of validation options
+    const names = ['alphanumeric', 'maxLength', 'minLength', 'whitespace'];
+
+    // Determine required values based on provided options or defaults
     const requiredOption = [
         options.minLength?.value || minLength.value,
         options.maxLength?.value || maxLength.value,
         options.alphanumeric?.required ?? alphanumeric.required,
         options.whitespace?.required ?? whitespace.required
-    ]
-    const message = [
-         options.minLength?.message || minLength.message,
-        options.maxLength?.message ||maxLength.message,
-        options.alphanumeric?.message || alphanumeric.message,
-        options.whitespace?.message || whitespace.message,
-    ]
-    const object = createValidationOptions(names,requiredOption,message)
-    return { ...object, ...options };
+    ];
 
+    // Define error messages for validation options
+    const message = [
+        options.minLength?.message || minLength.message,
+        options.maxLength?.message || maxLength.message,
+        options.alphanumeric?.message || alphanumeric.message,
+        options.whitespace?.message || whitespace.message
+    ];
+
+    // Create validation options object
+    const object = createValidationOptions(names, requiredOption, message);
+
+    // Merge with provided options
+    return { ...object, ...options };
 }
 
-function optionPassword(options){
-    const {lowercase,maxLength,minLength,number,specialCharacter,uppercase,whitespace } = defaultOptions.Password
+/**
+ * Generate validation options for password based on provided options and default settings.
+ * @param {Object} options - Options for customizing password validation criteria.
+ * @returns {Object} - Object containing validation options for password.
+ */
+function optionPassword(options) {
+    // Extract default settings for password validation
+    const { lowercase, maxLength, minLength, number, specialCharacter, uppercase, whitespace } = defaultOptions.Password;
+
+    // Define names of validation options
     const optionName = ['lowercase', 'maxLength', 'minLength', 'number', 'specialCharacter', 'uppercase', 'whitespace'];
+
+    // Determine required values based on provided options or defaults
     const optionValidations = [
         options.minLength?.value || minLength.value,
         options.maxLength?.value || maxLength.value,
@@ -38,44 +57,66 @@ function optionPassword(options){
         options.number?.required ?? number.required,
         options.specialCharacter?.required ?? specialCharacter.required,
         options.whitespace?.required ?? whitespace.required
-    ]
+    ];
+
+    // Define error messages for validation options
     const msgError = [
         options.minLength?.message || minLength.message,
-        options.maxLength?.message ||maxLength.message,
+        options.maxLength?.message || maxLength.message,
         options.uppercase?.message || uppercase.message,
         options.lowercase?.message || lowercase.message,
         options.number?.message || number.message,
         options.specialCharacter?.message || specialCharacter.message,
         options.whitespace?.message || whitespace.message
     ];
-    const objectOption = createValidationOptions(optionName,optionValidations,msgError)
+
+    // Create validation options object
+    const objectOption = createValidationOptions(optionName, optionValidations, msgError);
+
+    // Merge with provided options
     return { ...objectOption, ...options };
 }
 
-function optionEmail (options){
-    // Set standard length
-  const standardMaxLength = 255;
-  const standardMinLength = 3
-  const optionName = ['minLenLocal', 'minLenDomain', 'minLenSubdomain', 'maxLenLocal', 'maxLenDomain', 'maxLenSubdomain'];
-  const optionValidations = [standardMinLength,standardMinLength,2, standardMaxLength,standardMaxLength,standardMaxLength];
-  const msgError = 
-  [
-      `Local part must be ${standardMinLength}-${standardMaxLength} characters.`,
-      `Domain part must be ${standardMinLength}-${standardMaxLength} characters.`,
-      `Subdomain must be ${standardMaxLength} characters.`,
-      `Local part must be at most ${standardMaxLength} characters.`,
-      `Domain part must be at most ${standardMaxLength} characters.`,
-      `Subdomain must be at most ${standardMaxLength} characters.`,
-  ]
-  var objectOption = createValidationOptions(optionName,optionValidations,msgError);
+/**
+ * Generate validation options for email based on provided options and standard settings.
+ * @param {Object} options - Options for customizing email validation criteria.
+ * @returns {Object} - Object containing validation options for email.
+ */
+function optionEmail(options) {
+    // Define standard length requirements for email parts
+    const standardMaxLength = 255;
+    const standardMinLength = 3;
 
-  objectOption = {  ...objectOption,...options, };
+    // Define names of validation options
+    const optionName = ['minLenLocal', 'minLenDomain', 'minLenSubdomain', 'maxLenLocal', 'maxLenDomain', 'maxLenSubdomain'];
 
-  const { minLenLocal, minLenDomain, minLenSubdomain, maxLenLocal, maxLenDomain, maxLenSubdomain } = objectOption;
-  return {minLenLocal,minLenDomain,minLenSubdomain,maxLenLocal,maxLenDomain,maxLenSubdomain}
+    // Define required values and error messages for validation options
+    const optionValidations = [standardMinLength, standardMinLength, 2, standardMaxLength, standardMaxLength, standardMaxLength];
+    const msgError = [
+        `Local part must be ${standardMinLength}-${standardMaxLength} characters.`,
+        `Domain part must be ${standardMinLength}-${standardMaxLength} characters.`,
+        `Subdomain must be ${standardMaxLength} characters.`,
+        `Local part must be at most ${standardMaxLength} characters.`,
+        `Domain part must be at most ${standardMaxLength} characters.`,
+        `Subdomain must be at most ${standardMaxLength} characters.`,
+    ];
+
+    // Create validation options object
+    let objectOption = createValidationOptions(optionName, optionValidations, msgError);
+
+    // Merge with provided options
+    objectOption = { ...objectOption, ...options };
+
+    // Extract specific options for email validation
+    const { minLenLocal, minLenDomain, minLenSubdomain, maxLenLocal, maxLenDomain, maxLenSubdomain } = objectOption;
+
+    // Return relevant email validation options
+    return { minLenLocal, minLenDomain, minLenSubdomain, maxLenLocal, maxLenDomain, maxLenSubdomain };
 }
+
+// Export functions for generating validation options
 module.exports = {
     optionUsername,
     optionPassword,
     optionEmail,
-};
+}
